4c7195b5671f62bc988eb51ce47a6c738e3a3a4b
munna <dev2.mitaliint@gmail.com>
2023-08-08 18:16:09 +0600

    creating tools api

--- utils/Extension.js
+++ utils/Extension.js
@@ -1,115 +1,156 @@
 export const hairExtension = [
   {
-    name: "Silk Seam Hair Extension",
+    name: 'Silk Seam Hair Extension',
     icons:
-      "https://www.bellamiprofessional.com/cdn/shop/products/Jet_Black_1_a64b6eae-cc2c-4c92-aa34-e606196fa50c_grande.jpg?v=1558331166",
+      'https://www.bellamiprofessional.com/cdn/shop/products/Jet_Black_1_a64b6eae-cc2c-4c92-aa34-e606196fa50c_grande.jpg?v=1558331166',
     items: [
       {
         name: 'PICCOLINA 120G 18" ASH BLONDE (60) HAIR EXTENSIONS',
-        price: "196.99",
+        price: '196.99',
         image: [
-          "../images/extension/silk/26.webp",
-          "https://www.bellamihair.com/cdn/shop/products/Jet_Black_1_4e41bf0b-9569-4bb5-8acd-5114967f9401.jpg?v=1690854906&width=463",
+          '../images/extension/silk/26.webp',
+          'https://www.bellamihair.com/cdn/shop/products/Jet_Black_1_4e41bf0b-9569-4bb5-8acd-5114967f9401.jpg?v=1690854906&width=463',
         ],
         size: '26"',
       },
       {
         name: 'PICCOLINA 120G 18" ASH BLONDE (80) HAIR EXTENSIONS',
-        price: "196.99",
+        price: '196.99',
         image: [
-          "../images/extension/silk/24.webp",
-          "https://www.bellamihair.com/cdn/shop/products/Rooted_Cool_Brown_Butter_Blonde_WEB_a8bb8245-c3a9-4ae0-bb2c-432ffb62d600.jpg?v=1690857089&width=69",
+          '../images/extension/silk/24.webp',
+          'https://www.bellamihair.com/cdn/shop/products/Rooted_Cool_Brown_Butter_Blonde_WEB_a8bb8245-c3a9-4ae0-bb2c-432ffb62d600.jpg?v=1690857089&width=69',
         ],
         size: '24"',
       },
       {
         name: 'PICCOLINA 120G 18" ASH BLONDE (60) HAIR EXTENSIONS',
-        price: "196.99",
+        price: '196.99',
         image: [
-          "../images/extension/silk/22.webp",
-          "https://www.bellamihair.com/cdn/shop/products/Rooted_Cool_Brown_Butter_Blonde_WEB_a8bb8245-c3a9-4ae0-bb2c-432ffb62d600.jpg?v=1690857089&width=69",
+          '../images/extension/silk/22.webp',
+          'https://www.bellamihair.com/cdn/shop/products/Rooted_Cool_Brown_Butter_Blonde_WEB_a8bb8245-c3a9-4ae0-bb2c-432ffb62d600.jpg?v=1690857089&width=69',
         ],
         size: '22"',
       },
       {
         name: 'PICCOLINA 120G 18" ASH BLONDE (60) HAIR EXTENSIONS',
-        price: "196.99",
+        price: '196.99',
         image: [
-          "../images/extension/silk/20.webp",
-          "https://www.bellamihair.com/cdn/shop/products/Rooted_Cool_Brown_Butter_Blonde_WEB_a8bb8245-c3a9-4ae0-bb2c-432ffb62d600.jpg?v=1690857089&width=69",
+          '../images/extension/silk/20.webp',
+          'https://www.bellamihair.com/cdn/shop/products/Rooted_Cool_Brown_Butter_Blonde_WEB_a8bb8245-c3a9-4ae0-bb2c-432ffb62d600.jpg?v=1690857089&width=69',
         ],
         size: '20"',
       },
       {
         name: 'PICCOLINA 120G 18" ASH BLONDE (60) HAIR EXTENSIONS',
-        price: "196.99",
+        price: '196.99',
         image: [
-          "../images/extension/silk/16.webp",
-          "https://www.bellamihair.com/cdn/shop/products/Rooted_Cool_Brown_Butter_Blonde_WEB_a8bb8245-c3a9-4ae0-bb2c-432ffb62d600.jpg?v=1690857089&width=69",
+          '../images/extension/silk/16.webp',
+          'https://www.bellamihair.com/cdn/shop/products/Rooted_Cool_Brown_Butter_Blonde_WEB_a8bb8245-c3a9-4ae0-bb2c-432ffb62d600.jpg?v=1690857089&width=69',
         ],
         size: '16"',
       },
       {
         name: 'PICCOLINA 120G 18" ASH BLONDE (60) HAIR EXTENSIONS',
-        price: "196.99",
+        price: '196.99',
         image: [
-          "../images/extension/silk/18.webp",
-          "https://www.bellamihair.com/cdn/shop/products/Rooted_Cool_Brown_Butter_Blonde_WEB_a8bb8245-c3a9-4ae0-bb2c-432ffb62d600.jpg?v=1690857089&width=69",
+          '../images/extension/silk/18.webp',
+          'https://www.bellamihair.com/cdn/shop/products/Rooted_Cool_Brown_Butter_Blonde_WEB_a8bb8245-c3a9-4ae0-bb2c-432ffb62d600.jpg?v=1690857089&width=69',
         ],
         size: '18"',
       },
     ],
   },
   {
-    name: "Classic Hair Extension",
+    name: 'Classic Hair Extension',
     icons:
-      "https://www.bellamiprofessional.com/cdn/shop/products/Jet_Black_1_a64b6eae-cc2c-4c92-aa34-e606196fa50c_grande.jpg?v=1558331166",
+      'https://www.bellamiprofessional.com/cdn/shop/products/Jet_Black_1_a64b6eae-cc2c-4c92-aa34-e606196fa50c_grande.jpg?v=1558331166',
 
     items: [
       {
-        image: ["../images/extension/classic/24-Magnifica.webp"],
+        image: ['../images/extension/classic/24-Magnifica.webp'],
         size: '24" - Magnifica',
       },
       {
-        image: ["../images/extension/classic/22-Bellissima.webp"],
+        image: ['../images/extension/classic/22-Bellissima.webp'],
         size: '22" - Bellissima',
       },
       {
-        image: ["../images/extension/classic/20-Bambina.webp"],
+        image: ['../images/extension/classic/20-Bambina.webp'],
         size: '20" - Bambina',
       },
       {
-        image: ["../images/extension/classic/18-Piccolina.webp"],
+        image: ['../images/extension/classic/18-Piccolina.webp'],
         size: '18" - Piccolina',
       },
       {
-        image: ["../images/extension/classic/22-Boo-Gatti.webp"],
+        image: ['../images/extension/classic/22-Boo-Gatti.webp'],
         size: '22" - Boo-Gatti',
       },
     ],
   },
   {
-    name: "Wings",
+    name: 'Wings',
     icons:
-      "https://www.bellamihair.com/cdn/shop/products/F-Giselle_9860-Web-R_1024x1024.jpg?v=1603820799",
+      'https://www.bellamihair.com/cdn/shop/products/F-Giselle_9860-Web-R_1024x1024.jpg?v=1603820799',
     items: [],
   },
   {
-    name: "Hair Tools",
+    name: 'Hair Tools',
     icons:
-      "https://www.bellamihair.com/cdn/shop/products/ScreenShot2023-06-09at3.20.54PM_1024x1024.png?v=1686350199",
+      'https://www.bellamihair.com/cdn/shop/products/ScreenShot2023-06-09at3.20.54PM_1024x1024.png?v=1686350199',
+    items: [
+      {
+        name: 'MOISTURE RESTORE CONDITIONER 8 OZ',
+        price: '196.99',
+        image: [
+          'https://www.bellamihair.com/cdn/shop/files/MOISTUR…E-CONDITIONER-8.5OZ_FRONT_1_350x.jpg?v=1689098843',
+          'https://www.bellamihair.com/cdn/shop/files/Moisture-Restore-Shampoo-Conditioner-8oz_700x1000px.jpg?v=1689638317&width=69',
+        ],
+      },
+      {
+        name: 'MOISTURE RESTORE CONDITIONER 33 OZ REFILL',
+        price: '196.99',
+        image: [
+          '	https://www.bellamihair.com/cdn/shop/files/MOISTUR…-CONDITIONER-REFILL_FRONT_3_350x.jpg?v=1689098746',
+          'https://www.bellamihair.com/cdn/shop/files/MOISTURE-RESTORE-SHAMPOO-REFILL_FRONT_2.jpg?v=1689098696&width=463',
+        ],
+      },
+      {
+        name: 'MOISTURE RESTORE CONDITIONER 8 OZ',
+        price: '196.99',
+        image: [
+          '	https://www.bellamihair.com/cdn/shop/files/MOISTUR…E-CONDITIONER-8.5OZ_FRONT_1_350x.jpg?v=1689098843',
+          'https://www.bellamihair.com/cdn/shop/files/MOISTURE-RESTORE-CONDITIONER-8.5OZ_FRONT_1.jpg?v=1689098843&width=463',
+        ],
+      },
+      {
+        name: 'MOISTURE RESTORE CONDITIONER 33 OZ REFILL',
+        price: '196.99',
+        image: [
+          '	https://www.bellamihair.com/cdn/shop/files/MOISTUR…-CONDITIONER-REFILL_FRONT_3_350x.jpg?v=1689098746',
+          'https://www.bellamihair.com/cdn/shop/files/MOISTURE-RESTORE-CONDITIONER-REFILL_FRONT_3.jpg?v=1689098746&width=463',
+        ],
+      },
+      {
+        name: 'BLONDE BRILLIANCE PURPLE SHAMPOO 8 OZ',
+        price: '196.99',
+        image: [
+          '	https://www.bellamihair.com/cdn/shop/files/BLONDE-…ANCE-SHAMPOO-8.5-OZ_FRONT_3_350x.jpg?v=1689099018',
+          'https://www.bellamihair.com/cdn/shop/files/BLONDE-BRILLIANCE-SHAMPOO-8.5-OZ_FRONT_3.jpg?v=1689099018&width=463',
+        ],
+      },
+    ],
+  },
+  {
+    name: 'Hair Accessories',
+    icons:
+      'https://www.bellamihair.com/cdn/shop/products/nylon-all-1_1024x1024.png?v=1606490087',
     items: [],
   },
   {
-    name: "Hair Accessories",
+    name: 'Hair Care',
     icons:
-      "https://www.bellamihair.com/cdn/shop/products/nylon-all-1_1024x1024.png?v=1606490087",
-    items: [],
-  },
-  {
-    name: "Hair Care",
-    icons:
-      "https://www.bellamihair.com/cdn/shop/files/TOTAL-DEFENSE-6OZ_FRONT_2_1512x.jpg?v=1689115588",
+      'https://www.bellamihair.com/cdn/shop/files/TOTAL-DEFENSE-6OZ_FRONT_2_1512x.jpg?v=1689115588',
     items: [],
   },
 ];
